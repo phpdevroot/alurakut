ef31e2543697c8d7c71c2f2148406de0d088345a
BoOoT <phpdevroot@gmail.com>
2021-07-18 21:13:29 -0300

    A5 - Ajuste login

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ import nookies from 'nookies';
 
 export default function LoginScreen() {
   const router = useRouter();
-  const [githubUser, setGithubUser] = React.useState('omariosouto');
+  const [githubUser, setGithubUser] = React.useState('phpdevroot');
 
   return (
     <main
